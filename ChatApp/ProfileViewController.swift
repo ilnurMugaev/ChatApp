3c61fabc54f85ee509952905f9f4250b5201d8b0
Ilnur Mugaev <mugaevis@gmail.com>
2021-02-22 15:32:13 +0300

    Profile screen design created.

--- ChatApp/ProfileViewController.swift
+++ ChatApp/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  ChatApp
 //
 //  Created by Ilnur Mugaev on 14.02.2021.
@@ -8,15 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProfileViewController: UIViewController {
+        
+    @IBOutlet private weak var avatarImageView: UIImageView!
+    @IBOutlet private weak var initialsLabel: UILabel!
+    @IBOutlet private weak var nameTextView: UITextView!
+    @IBOutlet private weak var descriptionTextView: UITextView!
+    @IBOutlet private weak var editButton: UIButton!
+        
+    private let logging = Logging.shared
     
-    let logging = Logging.shared
+    private let userName = "Ilnur Mugaev"
+    private let userDescription = "iOS Developer\nKazan"
+        
+    private lazy var userInitials: String = {
+        let nameComponents = userName.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: " ")
+        let userInitials = nameComponents.reduce("") { ($0 == "" ? "" : "\($0.first ?? Character(" "))") + "\($1.first ?? Character(" "))" }
+        return userInitials.trimmingCharacters(in: .whitespacesAndNewlines)
+    }()
     
+    private let buttonCornerRadius: CGFloat = 14
+        
     // Срабатывает после загрузки view.
     override func viewDidLoad() {
         super.viewDidLoad()
         
         logging.printLog()
+        
+        nameTextView.text = userName
+        descriptionTextView.text = userDescription
+        initialsLabel.text = userInitials
     }
     
     // Срабатывает перед появлением view на экране.
@@ -38,6 +59,10 @@ class ViewController: UIViewController {
     
     // Срабатывает после того, как размер view изменился под размер экрана.
     override func viewDidLayoutSubviews() {
+        editButton.layer.cornerRadius = buttonCornerRadius
+        avatarImageView.layer.cornerRadius = avatarImageView.bounds.height / 2
+        let initialsFonSize = avatarImageView.bounds.height / 2
+        initialsLabel.font = UIFont.systemFont(ofSize: initialsFonSize)
         logging.printLog()
     }
         
@@ -53,4 +78,3 @@ class ViewController: UIViewController {
         logging.printLog()
     }
 }
-
